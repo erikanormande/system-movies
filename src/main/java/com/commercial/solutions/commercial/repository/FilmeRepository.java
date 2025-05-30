f445bc5a647b58e85e93fd29afc16f14430b179e
Erika <2022436@student.cct.ie>
2025-05-30 02:52:09 -0300

    add movie repository

---
+++ src/main/java/com/commercial/solutions/commercial/repository/FilmeRepository.java
@@ -0,0 +1,6 @@
+package com.commercial.solutions.commercial.repository;
+
+import com.commercial.solutions.commercial.model.Filme;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmeRepository extends JpaRepository<Filme, Long> {}
