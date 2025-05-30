72ef3c37dc374a0e5985033312dfc4fda9552428
Erika <2022436@student.cct.ie>
2025-05-30 02:45:16 -0300

    including import controller

---
+++ src/main/java/com/commercial/solutions/commercial/controller/ImportacaoController.java
@@ -0,0 +1,22 @@
+package com.commercial.solutions.commercial.controller;
+
+import com.commercial.solutions.commercial.service.ImportacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ImportacaoController {
+
+    @Autowired
+    private ImportacaoService importacaoService;
+
+    @PostMapping("/importar-csv")
+    public ResponseEntity<String> importarCsv() {
+        importacaoService.importarCsv();
+        return ResponseEntity.ok("Importação realizada com sucesso!");
+    }
+}
