1e55698b6c64fbf35f7cdc2fb294cba9edef8793
Erika <2022436@student.cct.ie>
2025-05-30 02:41:02 -0300

    including configuration files

---
+++ src/main/java/com/commercial/solutions/commercial/CommercialSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.commercial.solutions.commercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercialSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commercial/solutions/commercial/CommercialSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commercial.solutions.commercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
